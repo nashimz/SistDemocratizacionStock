8975a685d3ad937435eb2cd222a45f540f805371
facusato <64222987+facusato@users.noreply.github.com>
2020-05-06 17:05:12 -0300

    falla al agregar jpa

--- src/main/java/com/unla/Grupo8OO22020/controllers/HomeController.java
+++ src/main/java/com/unla/Grupo8OO22020/controllers/HomeController.java
@@ -3,7 +3,7 @@ package com.unla.Grupo8OO22020.controllers;
 //import org.springframework.security.core.context.SecurityContextHolder;
 //import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
