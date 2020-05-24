513ca925acd0128400bfa8403c6f122f6bcc9b51
barbara <barbara@grava.digital>
2020-05-24 20:21:48 -0300

    agrega pedido si el stock esta disponible

--- src/main/java/com/unla/Grupo8OO22020/controllers/PedidoController.java
+++ src/main/java/com/unla/Grupo8OO22020/controllers/PedidoController.java
@@ -1,6 +1,10 @@
 package com.unla.Grupo8OO22020.controllers;
 
 
+import java.util.ArrayList;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,8 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import com.unla.Grupo8OO22020.entities.Batch;
 import com.unla.Grupo8OO22020.helpers.ViewRouteHelper;
+import com.unla.Grupo8OO22020.models.BatchModel;
 import com.unla.Grupo8OO22020.models.PedidoModel;
+import com.unla.Grupo8OO22020.models.ProductModel;
+import com.unla.Grupo8OO22020.services.IBatchService;
 import com.unla.Grupo8OO22020.services.IPedidoService;
 import com.unla.Grupo8OO22020.services.IProductService;
 import com.unla.Grupo8OO22020.services.IStoreService;
@@ -37,7 +45,9 @@ public class PedidoController {
 	@Qualifier("storeService")
 	private IStoreService storeService;
 	
-	
+	@Autowired
+	@Qualifier("batchService")
+	private IBatchService batchService;
 	
 	@GetMapping("")
 	public ModelAndView index() {
@@ -57,8 +67,14 @@ public class PedidoController {
 	
 	@PostMapping("/create")
 	public RedirectView create(@ModelAttribute("pedido") PedidoModel pedidoModel) {
-		pedidoService.insert(pedidoModel);
+		
+		if(validarConsumo(productService.findByIdProduct(pedidoModel.getProduct().getIdProduct()), pedidoModel.getQuantity(), pedidoModel.getStore().getIdStore())){
+			pedidoService.insert(pedidoModel);
+			consumoStock(productService.findByIdProduct(pedidoModel.getProduct().getIdProduct()),pedidoModel.getQuantity(),pedidoModel.getStore().getIdStore());
+			
+		}
 		return new RedirectView(ViewRouteHelper.PEDIDO_ROOT);
+		
 	}
 	
 	@GetMapping("/{idPedido}")
@@ -81,5 +97,57 @@ public class PedidoController {
 		public RedirectView delete(@PathVariable("idPedido") long idPedido) {
 			pedidoService.remove(idPedido);
 			return new RedirectView(ViewRouteHelper.PEDIDO_ROOT);
+		}
+		
+		public List<Batch> traerLotesProducto(ProductModel productoModel, long idLocal) {
+			List<Batch> lotesActivos = new ArrayList<Batch>();
+			for (Batch b : batchService.getAll()) {
+				if (b.getProduct().getIdProduct() == productoModel.getIdProduct() && b.getStore().getIdStore() == idLocal ) {
+					lotesActivos.add(b);
+				}
+			}
+			return lotesActivos;
+		}
+
+		
+		public int calcularStock(ProductModel productoModel,long idLocal) {
+			int total = 0;
+			for (Batch b : traerLotesProducto(productoModel, idLocal)) {
+				total += b.getQuantities();
+			}
+			return total;
+		}
+
+		public boolean validarConsumo(ProductModel productoModel,int cantidad, long idLocal) {
+			return (calcularStock(productoModel, idLocal) >= cantidad) ? true : false; 
+		}
+		
+		public void consumoStock(ProductModel productModel, int cantidad, long idLocal ) {
+			int aux = cantidad;
+			int x = 0;
+
+			while (x < traerLotesProducto(productModel, idLocal).size() && aux > 0) {
+				Batch b = traerLotesProducto(productModel, idLocal).get(x);
+
+				if (b.getQuantities() > aux) {
+					b.setQuantities(b.getQuantities() - aux);
+					aux = 0;
+				} else if (b.getQuantities() < aux) {
+					aux -= b.getQuantities();
+					b.setQuantities(0);
+					
+				} else if (b.getQuantities() == aux) {
+					aux = 0;
+					b.setQuantities(0);
+					
+				}
+				BatchModel bM = batchService.findByIdBatch(b.getIdBatch());
+				bM.setQuantities(b.getQuantities());
+				
+				batchService.insert(bM);
+				x++;
+
+			}
+
 		}
 }
